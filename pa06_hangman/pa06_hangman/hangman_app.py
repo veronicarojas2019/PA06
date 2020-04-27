9c2251d42e152ed34a6676663941673cb32bae28
clairerousell <62521435+clairerousell@users.noreply.github.com>
2020-04-27 15:56:18 -0400

    Update hangman_app.py

--- pa06_hangman/pa06_hangman/hangman_app.py
+++ pa06_hangman/pa06_hangman/hangman_app.py
@@ -1,3 +1,4 @@
+ 
 """
    Authors: Claire Rousell, Kayla Romero, Maya Rosenfeld, Helen Lin, Veronica Rojas
    hangman_app.py is an app for playing hangman in the terminal
@@ -32,7 +33,7 @@ def play_hangman():
             else:
                 guessed_letters.append(letter)
                 print("The letter is in the word")
-            if len(correct_letters)==len(word):
+            if len(correct_letters) == len(word):
                 done = True
                 print("Congratulations, you won!")
             elif guesses_left==0:
@@ -44,7 +45,7 @@ def play_hangman():
                          print(word[i])
                      else:
                          print('__')
-                         letter = input("Enter a letter: ")
+               letter = input("Enter a letter: ")
         want_to_play = input("Would you like to play another game? [Y]es or [N]o")
 
 
