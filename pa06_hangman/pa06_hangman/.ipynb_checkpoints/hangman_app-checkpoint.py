ffea228a48c664d578aeae997a0fa36d4e5901d2
mayar <62971836+mayar266@users.noreply.github.com>
2020-04-29 19:15:31 -0400

    .

---
+++ pa06_hangman/pa06_hangman/.ipynb_checkpoints/hangman_app-checkpoint.py
@@ -0,0 +1,53 @@
+"""
+   Authors: Claire Rousell, Kayla Romero, Maya Rosenfeld, Helen Lin, Veronica Rojas
+   hangman_app.py is an app for playing hangman in the terminal
+   it is also used as a module in the hangman_webapp flask app
+"""
+import random
+words="These are some words that we can change later".split()
+
+def generate_random_word():
+   """ read a list of words from a file and pick a random one to return """
+   return random.choice(words)
+
+def play_hangman():
+    want_to_play = True
+
+
+    while (want_to_play):
+        guessed_letters = []
+        correct_letters = []
+        guesses_left = 6
+        word = generate_random_word()
+        letter = input("Enter a letter: ")
+        done = False
+        while not done:
+            if letter in guessed_letters:
+                guesses_left=guesses_left-1
+                print("You already guessed that letter")
+            elif letter not in word:
+                guessed_letters.append(letter)
+                print("The letter is not in the word")
+                guesses_left=guesses_left-1
+            else:
+                guessed_letters.append(letter)
+                print("The letter is in the word")
+            if len(correct_letters)==len(word):
+                done = True
+                print("Congratulations, you won!")
+            elif guesses_left==0:
+                done = True
+                print("Sorry, you lost!")
+            else:
+                for i in range(len(word)):
+                     if(word[i] in guessed_letters):
+                         print(word[i])
+                     else:
+                         print('__')
+                         letter = input("Enter a letter: ")
+        want_to_play = input("Would you like to play another game? [Y]es or [N]o")
+
+
+
+if __name__ == '__main__':
+    play_hangman()
---
+++ pa06_hangman/pa06_hangman/.ipynb_checkpoints/hangman_webapp-checkpoint.py
@@ -0,0 +1,53 @@
+"""
+  website_demo shows how to use templates to generate HTML
+  from data selected/generated from user-supplied information
+"""
+
+from flask import Flask, render_template, request
+import hangman_app
+app = Flask(__name__)
+
+global state
+state = {'guesses':[],
+         'word':"interesting",
+		 'word_so_far':"-----------",
+		 'done':False}
+
+@app.route('/')
+@app.route('/main')
+def main():
+	return render_template('hangman.html')
+
+@app.route('/start')
+def play():
+	global state
+	state['word']=hangman_app.generate_random_word()
+	state['guesses'] = []
+	return render_template("start.html",state=state)
+
+@app.route('/play',methods=['GET','POST'])
+def hangman():
+	""" plays hangman game """
+	global state
+	if request.method == 'GET':
+		return play()
+
+	elif request.method == 'POST':
+		letter = request.form['guess']
+        if letter in guessed_letters:
+            print("You have already guessed this letter, guess again.")
+        else:
+            if letter in word:
+                print("Good guess, that letter is in the word!")
+                if done==True:
+                    print("The word is complete, you won!")
+            if letter not in word:
+                print("That letter is not in the word. Guess again.")
+		state['guesses'] += [letter]
+		return render_template('play.html',state=state)
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=3000)
